4c0b4e07a2ab0c5501acd2e9bfef85a169171472
AbduAllah Gabbar <you@example.com>
2021-11-16 09:36:17 +0200

    #

---
+++ web/build/precache-manifest.84080e7fcefc57a6ff4cf490e526dbec.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4959d288bda20f31c3d1523d7c086cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978d3204c53300aeb7f4",
+    "url": "/static/css/2.aa9c7ed0.chunk.css"
+  },
+  {
+    "revision": "3a95f715be6ba17e37fa",
+    "url": "/static/css/main.99ec62b8.chunk.css"
+  },
+  {
+    "revision": "978d3204c53300aeb7f4",
+    "url": "/static/js/2.15fbf74d.chunk.js"
+  },
+  {
+    "revision": "e932298cc331ca7c0f0debd0067e2073",
+    "url": "/static/js/2.15fbf74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a95f715be6ba17e37fa",
+    "url": "/static/js/main.9d6b3089.chunk.js"
+  },
+  {
+    "revision": "d3ff7edbf14d41765782",
+    "url": "/static/js/runtime-main.31121ff7.js"
+  },
+  {
+    "revision": "43e31ceabf069c3846bbad74f061d3ef",
+    "url": "/static/media/DarislyIcon.43e31cea.png"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "4c9d1623b8f1e504d8dc313748c90a30",
+    "url": "/static/media/app-logo-english.4c9d1623.png"
+  },
+  {
+    "revision": "dccbcd6bbc8bcedd79ebdf600caf3ad6",
+    "url": "/static/media/banner-bg.dccbcd6b.png"
+  },
+  {
+    "revision": "12adc6233e867bd692880dd8a01822dc",
+    "url": "/static/media/banner-image.12adc623.png"
+  },
+  {
+    "revision": "5242645d061d7ab405d3355ff1d56f48",
+    "url": "/static/media/contact-image.5242645d.jpg"
+  },
+  {
+    "revision": "3abd4aeefd98e24ec4b295faed184e69",
+    "url": "/static/media/course-bg.3abd4aee.png"
+  },
+  {
+    "revision": "bc0bf49823b4cb3848f69664fedbad59",
+    "url": "/static/media/document.bc0bf498.png"
+  },
+  {
+    "revision": "e7da1c2eb69a714b13e208ed6ad87a78",
+    "url": "/static/media/egy-ar-language.e7da1c2e.png"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "6fb2b19d83c08192b8a128f7dec48aef",
+    "url": "/static/media/instructor1.6fb2b19d.png"
+  },
+  {
+    "revision": "883a0bbd11261ccb9ef55a4dbf17803b",
+    "url": "/static/media/instructor2.883a0bbd.png"
+  },
+  {
+    "revision": "a2367f28eab1f8121a2feedeeea9f096",
+    "url": "/static/media/instructor3.a2367f28.png"
+  },
+  {
+    "revision": "79023643c261f9dce7e692d8e81e7558",
+    "url": "/static/media/instructor4.79023643.png"
+  },
+  {
+    "revision": "110842ad24e0c0cd4a6f0a5a909b739c",
+    "url": "/static/media/instructor5.110842ad.png"
+  },
+  {
+    "revision": "8f93ff18c4848c4347b109e73f9391fa",
+    "url": "/static/media/instructor6.8f93ff18.png"
+  },
+  {
+    "revision": "a77944f7dcbca2ed56a7f8f4997756d3",
+    "url": "/static/media/instructor7.a77944f7.png"
+  },
+  {
+    "revision": "c765b181e5976b64115a9334f91a3cbb",
+    "url": "/static/media/instructor8.c765b181.png"
+  },
+  {
+    "revision": "e53c3c353e3f7688e45007eb9a951175",
+    "url": "/static/media/mathematics.e53c3c35.png"
+  },
+  {
+    "revision": "3a4394074ddf1166f67e6a4cf1612c80",
+    "url": "/static/media/mcq.3a439407.png"
+  },
+  {
+    "revision": "bdbd6b36dbb44efd70932cf6a32de46e",
+    "url": "/static/media/partner-image.bdbd6b36.png"
+  },
+  {
+    "revision": "d9ee2f4d1026014507cad54f19bb4222",
+    "url": "/static/media/video-image.d9ee2f4d.png"
+  },
+  {
+    "revision": "854d0bf66e5ab67d782644309be86109",
+    "url": "/static/media/white-logo.854d0bf6.png"
+  }
+]);
