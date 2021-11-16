0e748fb156c9d9a59f0428868d8be01280952dcf
AbduAllah Gabbar <you@example.com>
2021-11-16 09:44:30 +0200

    abdallah-15-10-2021

---
+++ admin/build/precache-manifest.f2e4724cb1ba40dd0ad70be6ee1f624c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb94d62e7513c3ed54fc7434927c325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc682c695187a3d49f3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83d91ba32eb8f2d297ec",
+    "url": "/static/js/2.76b5e0cc.chunk.js"
+  },
+  {
+    "revision": "980b85962847aa8967aae5074f7fd5d5",
+    "url": "/static/js/2.76b5e0cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc682c695187a3d49f3",
+    "url": "/static/js/main.e7ce4670.chunk.js"
+  },
+  {
+    "revision": "6a59bd51eb184ae677f3",
+    "url": "/static/js/runtime-main.836b504c.js"
+  }
+]);
